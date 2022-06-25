fd51a77a5049ebe96a0ba6b0b4b20719458d5cbd
Shamkhal Guliyev <shamxal.guliyev91@gmail.com>
2022-06-25 11:47:14 +0400

    project setup

---
+++ LectureProject_1/LectureProject_1/CityListController.swift
@@ -0,0 +1,20 @@
+//
+//  CityListController.swift
+//  LectureProject_1
+//
+//  Created by Shamkhal Guliyev on 25.06.22.
+//
+
+import UIKit
+
+class CityListController: UIViewController {
+
+    let cityList = ["aze": ["Baku", "Ganja", "Shusha"],
+                    "tur": ["Istanbul", "Izmir", "Mugla"],
+                    "usa": ["Los Angeles", "New York", "Boston"]]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
